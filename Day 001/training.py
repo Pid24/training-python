2d885517d64b58b46ef2a9b028e11bf12b99fe8e
Pid24 <rofidnasifannafie@gmail.com>
2024-07-22 18:00:49 +0700

    day-001

---
+++ Day 001/training.py
@@ -0,0 +1,26 @@
+# print("Hello World!") 
+# print("Hello World!") 
+# print("Hello World!")
+
+# print("Hello World!\nHello World!\nHello World!")
+
+# print("Hello" + " " + "Angela")
+
+# input("What is your name?") 
+
+# print("Hello " + input("What is your name?"))
+
+# name = input("What is your name?")
+# print(name)
+
+# name = "Rofid"
+# print(name)
+
+# name = "Yuna"
+# print(name)
+
+# print( len( input("What is your name?") ) )
+
+# name = input("What is your name?")
+# length = len(name)
+# print(length)
