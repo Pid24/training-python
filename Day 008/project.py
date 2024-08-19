bd335b5f3ee24d18af319160c90f693a473829fd
Pid24 <rofidnasifannafie@gmail.com>
2024-08-19 15:53:17 +0700

    Day 008

---
+++ Day 008/project.py
@@ -0,0 +1,40 @@
+def caesar_cipher(text, shift, direction):
+    result = ""
+    if direction == "decode":
+        shift *= -1
+    for char in text:
+        if char.isalpha():
+            shift_amount = 65 if char.isupper() else 97
+            result += chr((ord(char) - shift_amount + shift) % 26 + shift_amount)
+        else:
+            result += char
+    return result
+
+print(r""",adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     'Y8 a8P_____88 I8[    "" ""     'Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  '"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ '"Ybbd8"' '"8bbdP"Y8  '"Ybbd8"' '"YbbdP"' '"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       d8 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ '"Ybbd8"' 88 88'YbbdP"'  88       88  '"Ybbd8"' 88          
+              88                                             
+              88           """)
+
+while True:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n")
+    shift = int(input("Type the shift number:\n"))
+
+    encrypted_message = caesar_cipher(text, shift, direction)
+    print(f"Here's the {direction}d result: {encrypted_message}")
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    if restart == 'no':
+        print("Goodbye")
+        break
---
+++ Day 008/training.py
@@ -0,0 +1,19 @@
+# def greet():
+#     print("Hello")
+#     print("Hello")
+#     print("Hello")
+#     print("Hello")
+
+# greet()
+
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}")
+
+# greet_with_name("Rofid")
+
+def greet_with(name, location):
+    print(f"hallo moms {name}")
+    print(f"staycation yuu moms di {location}")
+
+greet_with("Amanda", "Bali") 
